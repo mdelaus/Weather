ee8bf22f7efc38c8fa6d9798974c7f7e70b19b82
Michael DeLaus <mdelaus@mit.edu>
2016-07-13 23:36:04 -0400

    added html fetcher

--- src/weather/Browser.java
+++ src/weather/Browser.java
@@ -1,7 +1,17 @@
 package weather;
+import java.net.*;
+import java.io.*;
 
 public class Browser {
-    public static void getWeather(){
+    public static void getWeather() throws Exception{
         System.out.println("Getting weather...");
+        URL oracle = new URL("http://www.weather-forecast.com/locations/boston/forecasts/latest");
+        BufferedReader in = new BufferedReader(
+        new InputStreamReader(oracle.openStream()));
+
+        String inputLine;
+        while ((inputLine = in.readLine()) != null)
+            System.out.println(inputLine);
+        in.close();
     }
 }
--- src/weather/Main.java
+++ src/weather/Main.java
@@ -1,7 +1,8 @@
 package weather;
 
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Browser.getWeather();
     }
 }
