d785c307ff3fdbb87898047412bac2983b01c157
Michael DeLaus <mdelaus@mit.edu>
2016-07-17 13:38:32 -0400

    interactive weather forcast output

--- src/weather/Browser.java
+++ src/weather/Browser.java
@@ -1,17 +1,41 @@
 package weather;
 import java.net.*;
 import java.io.*;
+import java.util.Scanner;
 
 public class Browser {
     public static void getWeather() throws Exception{
+        
         System.out.println("Getting weather...");
-        URL oracle = new URL("http://www.weather-forecast.com/locations/boston/forecasts/latest");
+        
+        //Scanner to read city input
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a city: ");
+        String city = scanner.next();
+        
+        //fetch the weather for a given city
+        URL oracle = new URL("http://www.weather-forecast.com/locations/"+ city + "/forecasts/latest");
         BufferedReader in = new BufferedReader(
         new InputStreamReader(oracle.openStream()));
-
+        
+        //Formats the weather forcast
+        String forcast = null;
         String inputLine;
-        while ((inputLine = in.readLine()) != null)
-            System.out.println(inputLine);
+        String replaced = null;
+        while ((inputLine = in.readLine()) != null) {
+            //System.out.println(inputLine);
+            
+            //Pulls the frocast from the websites HTML
+            if (inputLine.contains("<span class=\"phrase\">")) {
+                String[] split = inputLine.split("<span class=\"phrase\">");
+                forcast = split[1].split("</span></span>")[0];
+                replaced = forcast.replace("&deg;", " °").replace(". ", "\n");
+            }
+        }
+        
+        //Print the weather forcast
+        System.out.println(replaced);
+            
         in.close();
     }
 }
